7b78d804e612574197ff7ea7564878f590b930bf
junwoo45 <sinyoorang@gmail.com>
2019-09-16 20:07:27 +0900

    mock데이터 생성
    *ngFor로 mock데이터 목록 표시

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Lion'},
+  { id: 11, name: 'Tiger'},
+  { id: 12, name: 'Eagle'},
+  { id: 13, name: 'Rabbit'},
+  { id: 14, name: 'Eagle'},
+  { id: 15, name: 'Cow'},
+  { id: 16, name: 'Giraff'},
+  { id: 17, name: 'Dog'},
+  { id: 18, name: 'Mouse'},
+  { id: 19, name: 'Hippo'},
+  { id: 20, name: 'Cheeta'},
+];
