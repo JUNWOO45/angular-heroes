4ae945b6d862b5ef961c8ad8d36cd79626314751
junwoo45 <sinyoorang@gmail.com>
2019-09-16 20:00:04 +0900

    angular가 ngModel 디렉티브를 제대로 인식하고 동작할 수 있도록 AppModule에 FormsModule로드

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { FormsModule } from '@angular/forms'; //NgModel을 사용하기위해서 로딩하였습니다.
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  //이 배열에는 어플리케이션이 동작할 때 필요한 외부 모듈을 등록합니다.
+    BrowserModule,
+    FormsModule //import한 FormsModule을 @NgModule메타데이터의 imports배열에 추가합니다.
   ],
   providers: [],
   bootstrap: [AppComponent]
