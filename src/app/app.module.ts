fd3d6ef06936fc90489cbe1d5c0838df3526dd26
junwoo45 <sinyoorang@gmail.com>
2019-09-17 10:31:57 +0900

    상세정보를 자식컴포넌트로 분리
    단방향 바인딩 사용하여 property전달

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-import { FormsModule } from '@angular/forms'; //NgModel을 사용하기위해서 로딩하였습니다.
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; //NgModel을 사용하기위해서 로딩하였습니다.
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [  //이 배열에는 어플리케이션이 동작할 때 필요한 외부 모듈을 등록합니다.
     BrowserModule,
