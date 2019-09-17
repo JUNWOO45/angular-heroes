ea8d0949f0bc9010019ae9a44730991aff7ea78d
junwoo45 <sinyoorang@gmail.com>
2019-09-17 21:05:50 +0900

    옵저버블 사용하여 비동기처리

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +15,26 @@ export class HeroesComponent implements OnInit {
     name: 'junwoo PARK'
   };
 
-  heroes = HEROES;
-
+  // heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
+
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(data => this.heroes = data);
+
   }
 
 }
