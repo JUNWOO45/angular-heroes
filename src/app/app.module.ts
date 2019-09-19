8d61091679a60bc559388a28ea26a3ea020e2de9
Parkjunwoo <sinyoorang@gmail.com>
2019-09-19 23:41:58 +0900

    라우팅 모듈 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; //NgModel을 사용하기위해서 로딩하였습니다.
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module'; //NgModel을 사용하기위해서 로딩하였습니다.
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { MessagesComponent } from './messages/messages.component'; //NgModel을 
   ],
   imports: [  //이 배열에는 어플리케이션이 동작할 때 필요한 외부 모듈을 등록합니다.
     BrowserModule,
-    FormsModule //import한 FormsModule을 @NgModule메타데이터의 imports배열에 추가합니다.
+    FormsModule,
+    AppRoutingModule //import한 FormsModule을 @NgModule메타데이터의 imports배열에 추가합니다.
   ],
   providers: [],
   bootstrap: [AppComponent]
