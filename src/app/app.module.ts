870a462b74a8a2ec8e467aee8d50d763e51828db
Parkjunwoo <sinyoorang@gmail.com>
2019-09-20 22:56:12 +0900

    Make in memory database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component'; //NgModel을 사용하기위해서 로딩하였습니다.
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [  //이 배열에는 어플리케이션이 동작할 때 필요한 외부 모듈을 등록합니다.
     BrowserModule,
     FormsModule,
-    AppRoutingModule //import한 FormsModule을 @NgModule메타데이터의 imports배열에 추가합니다.
+    AppRoutingModule, //import한 FormsModule을 @NgModule메타데이터의 imports배열에 추가합니다.
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
